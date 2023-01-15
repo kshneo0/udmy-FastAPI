b5ea26d8fe11951ab5279e5084b5a89c943675aa
kshneo0 <kshneo@gmail.com>
2023-01-15 17:18:06 +0900

    Book2 Project - create BaseModel

--- books2.py
+++ books2.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID
+
 
 app = FastAPI()
 
 
+class Book(BaseModel):
+    id: UUID
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
 BOOKS = []
+
 
 @app.get("/")
 async def read_all_books():
