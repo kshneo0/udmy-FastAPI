986a1ae0290710533fe48d1a040d243337765a5f
kshneo0 <kshneo@gmail.com>
2023-01-15 16:20:20 +0900

    FastAPI - Post Request

--- books.py
+++ books.py
@@ -35,6 +35,20 @@ async def read_book(book_name: str):
     return BOOKS[book_name]
 
 
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
+
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
+
+    BOOKS[f'book_{current_book_id + 1}'] = {'title': book_title, 'author': book_author}
+    return BOOKS[f'book_{current_book_id + 1}']
+
+
 @app.get("/directions/{direction_name}")
 async def get_direction(direction_name: DirectionName):
     if direction_name == DirectionName.north:
