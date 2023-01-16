f8367a18e083b1358cfab0b8d0a9135f14a1192a
kshneo0 <kshneo@gmail.com>
2023-01-17 05:22:16 +0900

    Todo Project - Main (Create Database Connection for API)

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-SQLALCHEMY_DATABASE_URL = "sqllite:///./todos.db"
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, content_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
