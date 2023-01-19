e6508c65f74fa4c710cf893e97183749dc23d23a
kshneo0 <kshneo@gmail.com>
2023-01-20 05:31:27 +0900

    Todo Project - Save User to Database

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional
 import models
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
 
 class CreateUser(BaseModel):
     username: str
@@ -14,14 +16,25 @@ class CreateUser(BaseModel):
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 
 def get_password_hash(password):
     return bcrypt_context.hash(password)
 
 
 @app.post("/create/user")
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
@@ -33,4 +46,7 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.hashed_password = hash_password
     create_user_model.is_active = True
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+    # return create_user_model
