7ea534032f7245cb2f54b0c5d969ee95ba7bc5e7
kshneo0 <kshneo@gmail.com>
2023-01-19 20:17:54 +0900

    Todo Project - Put Request (Todo Project)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -18,11 +18,13 @@ def get_db():
     finally:
         db.close()
 
+
 class Todo(BaseModel):
     title: str
     description: Optional[str]
     priority: int = Field(gt=0, lt=6, description="The priority must be between 1-5")
     complete: bool
+
 
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
@@ -50,6 +52,27 @@ async def create_todo(todo: Todo, db: Session = Depends(get_db)):
 
     return {
         'status': 201,
+        'transaction': 'Successful'
+    }
+
+
+@app.put("/{todo_id}")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_exception()
+
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 200,
         'transaction': 'Successful'
     }
 
